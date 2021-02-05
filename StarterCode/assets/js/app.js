ac10eb47865c8bbde916b43ce9ecf98897789cf5
Moja One <joalex09@gmail.com>
2021-02-04 23:53:45 -0500

    svg code creation

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,14 @@
-// @TODO: YOUR CODE HERE!
+//Data Journalism
+
+// The code for the chart is wrapped inside a function that
+// automatically resizes the chart
+function makeResponsive() {
+
+    // if the SVG area isn't empty when the browser loads,
+  // remove it and replace it with a resized version of the chart
+  var svgArea = d3.select("body").select("svg");
+
+   // clear svg is not empty
+   if (!svgArea.empty()) {
+    svgArea.remove();
+  }
